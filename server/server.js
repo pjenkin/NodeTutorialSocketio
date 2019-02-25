54358819e07de3527cd783eab4ee36c4b365f138
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-25 15:26:03 +0000

    socket.io event listening on both client & server - challenge 9-107

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
@@ -14,6 +16,11 @@ console.log('publicPath using path.join : ',publicPath);
 // challenge 9-106: express app, middleware to server public, listen on 3000 (with message)
 
 var app = express();
+// var server = http.createServer((request, response) => { });
+var server = http.createServer(app);    // can even use express variable as http module argument
+var io = socketIO(server);
+
+
 // var port = 3000;      // just 3000 for now
 const port = process.env.PORT || 3000;
 
@@ -25,9 +32,18 @@ app.get('/public', (request, response) =>
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) =>
+{
+  console.log('new user connected')
+  // challenge 9-107
+  socket.on('disconnect', () =>
+  {
+    console.log('a user disconnected');
+  });
+});
 
 
-app.listen(port, () =>
+server.listen(port, () =>
 {
   console.log(`Started on port ${port}`);
 });
