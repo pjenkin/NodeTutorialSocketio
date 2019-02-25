b858881d8b32f89837dab1cd912bd1483ca6b6ab
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-25 10:56:59 +0000

    9-106 path.join used to derive tidier relative path

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
-// = require()
+const path = require('path');
 
-console.log(__dirname + '/../public');   // current directory + public
+const publicPath = path.join(__dirname, '../public');
+
+console.log('using dot dot only', __dirname + '/../public');   // current directory + public
 // C:\Users\peter.DESKTOP-3GCVT7E\source\repos\Node\NodeChatApp\server/../public
 // awkward looking directory hopping because of ''..''
+console.log('publicPath using path.join : ',publicPath);
+// C:\Users\peter.DESKTOP-3GCVT7E\source\repos\Node\NodeChatApp\public
+// tidier path using path.join
