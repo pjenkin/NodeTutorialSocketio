cc90a0823a0be647a98a9a44bfaa67790b4a0d93
Peter N Jenkin <peternjenkin@gmail.com>
2019-02-25 10:52:50 +0000

    9-106 awkward directory hopping with ..

---
+++ server/server.js
@@ -0,0 +1,5 @@
+// = require()
+
+console.log(__dirname + '/../public');   // current directory + public
+// C:\Users\peter.DESKTOP-3GCVT7E\source\repos\Node\NodeChatApp\server/../public
+// awkward looking directory hopping because of ''..''
