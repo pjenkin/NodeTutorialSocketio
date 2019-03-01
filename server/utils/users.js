a7b2328e5fd08e8392ed43b1dc7515104c3359a1
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-01 14:39:06 +0000

    addUser, removeUser, getUser and getUserList, and tests for same using .filter and .map including challenge 9-125

--- server/utils/users.js
+++ server/utils/users.js
@@ -26,6 +26,62 @@ class Users
     this.users.push(user);
     return user;
   }
+
+  // challenges 9-125: removeUser and getUser
+
+  removeUser (id)
+  {
+    // removeUser by filtering out the specified user
+    // there may be a quicker/neater way
+    // could've used splice or _.remove or other means https://love2dev.com/blog/javascript-remove-from-array/
+    let removedUser = this.users.filter((user) => user.id === id);
+    let filteredUsers = this.users.filter((user) => user.id !== id);
+    this.users = filteredUsers;       /* non-matching filter will return empty array */
+    if (removedUser.length > 0)
+      return removedUser[0];
+    else
+      return null;
+    // in my solution, I'm indexing 1st (only) of array if array has any content
+    // return user which has been removed - for undo &c
+  }
+
+  getUser(id)
+  {
+    let gottenUser = this.users.filter((user) => user.id === id);
+    if (gottenUser.length > 0)
+      return gottenUser[0];
+    else
+      return null;
+
+    /* non-matching filter will return empty array */
+  }
+
+  getUserList(roomName)
+  {
+    // return array of strings of names
+    let users = this.users.filter((user) => user.roomName === roomName);
+    // ES6 shorthand
+    // {
+    //     // return true to keep in array or false to remove from array
+    //     return user.roomName === roomName;
+    // });
+    let namesArray = users.map((user) => user.name);
+    // ES6 shorthand
+
+    // {
+    //   return user.name;
+    // });
+    // return only names, using array.map
+
+    return namesArray;
+  }
+
+  // use 'get' keyword to bind function as a run-time evaluated getter property
+  get length()
+  {
+    return this.users.length;   // constructor d'initialise with empty array (length: 0)
+  }
+
 }
 
 module.exports = {Users};
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -3,6 +3,31 @@ const {Users} = require('./users');
 
 describe('Users', () =>
 {
+
+  let users;  // should be accessible inside beforeEach and within each expect test case
+
+  beforeEach(() =>
+  {
+      users = new Users();
+      users.users = [
+        {
+          id: 1,
+          name: 'PNJ Annear',
+          roomName: 'Node Course'
+        },
+        {
+          id: 2,
+          name: 'JNP Jones',
+          roomName: 'C++ Course'
+        },
+        {
+          id: 3,
+          name: 'NPJ Murdoch',
+          roomName: 'Node Course'
+        },
+      ];
+  });
+
   it('should add new user', () =>
   {
     let users = new Users();
@@ -15,6 +40,66 @@ describe('Users', () =>
     let resUser = users.addUser(user.id, user.name, user.roomName);
 
     expect(users.users).toEqual([user]);
-    
+
+  });
+
+  // challenges 9-125 (x4)
+
+  it('should remove a user', () =>
+  {
+    // take an id, remove & assert removal (removed user returned and users array length decremented)
+    let usersLength = users.length;
+    let removedUser = users.removeUser(1);
+
+    expect(removedUser).toEqual({
+      id: 1,
+      name: 'PNJ Annear',
+      roomName: 'Node Course'
+    });
+    expect(users.length).toEqual(usersLength - 1);
+  });
+
+  it('should not remove a user', () =>
+  {
+    // take an invalid id, remove & assert no removal (ie null returned and length unchanged)
+    let usersLength = users.length;
+    let removedUser = users.removeUser(99);
+
+    expect(removedUser).toEqual(null);
+    expect(users.length).toEqual(usersLength);
+  });
+
+  it('should find a user', () =>
+  {
+    // take an id, find & assert return
+    let gottenUser = users.getUser(1);
+
+    expect(gottenUser).toEqual({
+      id: 1,
+      name: 'PNJ Annear',
+      roomName: 'Node Course'
+    });
+  });
+
+  it('should not find a user', () =>
+  {
+      // take an invalid id, try to find & assert not found
+      let gottenUser = users.getUser(99);
+
+      expect(gottenUser).toEqual(null);
+  });
+
+
+  it('should return names with argument as \'Node Course\'',() =>
+  {
+    let userList = users.getUserList('Node Course');
+    expect(userList).toEqual(['PNJ Annear', 'NPJ Murdoch']);
+  });
+
+  it('should return names with argument as \'C++ course\'',() =>
+  {
+    let userList = users.getUserList('C++ Course');
+
+    expect(userList).toEqual(['JNP Jones']);
   });
 });
